f3558e5a116662cbd3634c475fa1667f1712bfbd
JINISHAM <jinishamurugesh@gmail.com>
2024-01-24 20:31:13 +0530

    react app commit

---
+++ src/components/functionalcomponents/testComponents.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import TestClassComp from "../classComponents/testClassComponents";
+
+function Testcomponents(){
+    return(
+        <div>
+            <TestClassComp/>
+            <h1>This is test functional component</h1>
+        </div>
+    );
+
+}
+
+export default Testcomponents;
+
+/*function add(a,b){
+    return a+b;
+}
+console.log(add(5,4))*/
